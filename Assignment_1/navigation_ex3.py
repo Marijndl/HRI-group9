a74508d23864c34ecf293985a997d9f36a93423b
Martin <94135385+martinkremnicky@users.noreply.github.com>
2024-05-07 11:35:32 +0200

    implement basic walk to target

--- Assignment_1/navigation_ex3.py
+++ Assignment_1/navigation_ex3.py
@@ -10,7 +10,7 @@ start_time = time.time() #the whole number, in seconds
 timeout_limit = 20 #in seconds
 timeout_time = start_time + timeout_limit
 
-robot_IP="192.168.0.102"
+# robot_IP="192.168.0.102"
 # robot_IP="127.0.0.1"
 
 nao.InitProxy(robot_IP)
@@ -25,9 +25,6 @@ def print_landmark_debug():
     detected, timestamp, markerInfo=nao.DetectLandMark()
     print(detected,markerInfo)
 
-
-
-
 def look_on_spot():
     found_bool, xvalue, sizeX = lookltr()
     if found_bool == False: #if not in view, turn around and try again
@@ -37,15 +34,23 @@ def look_on_spot():
 
 def lookltr():
     yaw_start = 2
+    yaw_end = -2
     yaw_counter = yaw_start
-    yaw_rate = 0.2
+    yaw_rate = 0.5
     found_bool = False
-    while (found_bool == False) or (yaw_counter<=(yaw_start - yaw_rate)):
-        #TODO: every 0.5 seconds
-        nao.MoveHead(yaw_val=yaw_counter)
-        found_bool, target_x_location, sizeX = is_target_found()
 
-        yaw_counter = yaw_counter - yaw_rate
+    start_time = time.time()
+    while ((found_bool == False) and (yaw_counter>=yaw_end)):
+        # print_landmark_debug()
+        current_time = time.time()
+        if current_time - start_time >= 1:
+            start_time = current_time
+            nao.MoveHead(yaw_val=yaw_counter)
+            found_bool, target_x_location, sizeX = is_target_found()
+            print(found_bool, yaw_counter,yaw_counter>=yaw_end)
+            yaw_counter = yaw_counter - yaw_rate
+        
+
     return found_bool, target_x_location, sizeX
 
 
@@ -68,7 +73,10 @@ def walk_to_target(target_x_location, sizeX, toggle=True):
         nao.Walk(0.,0.,target_x_location) #how to convert target_x_location to angles?
 
         target_distance = get_target_distance(sizeX)
-        nao.Walk(target_distance - safety_margin, 0.,0.)
+        print("target_distance: ",target_distance)
+        # nao.Walk(target_distance - safety_margin, 0.,0.)
+        nao.Walk(.5, 0.,0.)
+
     else:
         nao.Move(0.0,0.0,0.0)
     
@@ -100,72 +108,82 @@ def avoid_obstacle(SL,SR, min_distance):
     elif SL>SR:
         if SR<min_distance:
             nao.Walk(0.,min_distance,0.)
-    #else walk either straight backwards or to a random diagonal backwards
+    else:
+        nao.Walk(-min_distance, 0.,0.)
+        #else walk either straight backwards or to a random diagonal backwards
 
 def is_obstacle_close(SL,SR, min_distance):
     if SL<min_distance or SR<min_distance:
         return True
     return False
 
+def is_at_target():
+    return get_target_distance() <= 0.5
 
 
 
 
-#TODO: finish impelemting 
 
 # finding target
 found_bool = False
 while found_bool == False:
+    # print_landmark_debug()
     found_bool, target_x_location, sizeX = look_on_spot()
-    change_location_random()
+    print("target found", found_bool, target_x_location, sizeX)
+    # change_location_random()
 
 walk__bool = True
 while walk__bool == True:
-    if not is_obstacle_close:
-        walk_to_target(target_x_location, sizeX)
-    else:
-        walk_to_target(target_x_location, sizeX, is_obstacle_close)
-        avoid_obstacle()
+    walk_to_target(target_x_location, sizeX)
+    # if not is_obstacle_close:
+    #     walk_to_target(target_x_location, sizeX)
+    # else:
+    #     walk_to_target(target_x_location, sizeX, is_obstacle_close)
+    #     avoid_obstacle()
+    if is_at_target():
+        nao.Move(0.,0.,0.)
+        walk__bool = False
+    
+
+#TODO: finish impelemting - rewrite below
+
+# last_execution_10 = None
+# last_execution_05 = None
+# last_execution_025 = None
+# # new_yaw = None
+# while start_time < timeout_time:
+#     elapsed_time = start_time - time.time() #time so far
+#     current_execution_10 = round(elapsed_time,1) #10HZ
+#     current_execution_05 = int(elapsed_time // 2) * 2 #0.5HZ -> every 2 seconds
+#     current_execution_025 = int(elapsed_time // 4) * 4 #0.25HZ -> every 4 seconds
 
 
 
-last_execution_10 = None
-last_execution_05 = None
-last_execution_025 = None
-# new_yaw = None
-while start_time < timeout_time:
-    elapsed_time = start_time - time.time() #time so far
-    current_execution_10 = round(elapsed_time,1) #10HZ
-    current_execution_05 = int(elapsed_time // 2) * 2 #0.5HZ -> every 2 seconds
-    current_execution_025 = int(elapsed_time // 4) * 4 #0.25HZ -> every 4 seconds
+
+#     #this will execute at a rate of 10Hz
+#     if current_execution_10 != last_execution_10:
+#         [SL, SR]=nao.ReadSonar() #meters
+#         # target_angle = get_target_angle() #
+#         detected, timestamp, markerInfo=nao.DetectLandMark()
+#         print(detected,markerInfo)
 
 
+#         #TODO: implement navigation to target 
+#         #TODO: implement obstacle avoidance
 
+#     #this will execute at a rate of 0.25Hz
+#     # if current_execution_025 != last_execution_025:
+#         # nao.Walk(0.1,0.,0.) #walk 10cm forward
+#         # nao.Move(0.1,0.,0) #move 10cm toward target? have to clarify meaning 
+#         # nao.Move(0.1,0.,target_angle) #move 10cm toward target? have to clarify meaning 
 
-    #this will execute at a rate of 10Hz
-    if current_execution_10 != last_execution_10:
-        [SL, SR]=nao.ReadSonar() #meters
-        # target_angle = get_target_angle() #
-        detected, timestamp, markerInfo=nao.DetectLandMark()
-        print(detected,markerInfo)
+#     # if current_execution_05 != last_execution_05:
+#     #     new_yaw = initial_yaw - 0.25
+#     #     looking(detected)
 
-
-        #TODO: implement navigation to target 
-        #TODO: implement obstacle avoidance
-
-    #this will execute at a rate of 0.25Hz
-    # if current_execution_025 != last_execution_025:
-        # nao.Walk(0.1,0.,0.) #walk 10cm forward
-        # nao.Move(0.1,0.,0) #move 10cm toward target? have to clarify meaning 
-        # nao.Move(0.1,0.,target_angle) #move 10cm toward target? have to clarify meaning 
-
-    # if current_execution_05 != last_execution_05:
-    #     new_yaw = initial_yaw - 0.25
-    #     looking(detected)
-
-    last_execution_10 = current_execution_10
-    last_execution_05 = current_execution_05
-    last_execution_025 = current_execution_025
+#     last_execution_10 = current_execution_10
+#     last_execution_05 = current_execution_05
+#     last_execution_025 = current_execution_025
 
 
 
