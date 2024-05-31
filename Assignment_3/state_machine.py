8b4f5204c97f10421b218d7b5b4f3128e70c7adb
Marijn <51094560+Marijndl@users.noreply.github.com>
2024-05-31 15:38:53 +0200

    Initial state machine

---
+++ Assignment_3/state_machine.py
@@ -0,0 +1,108 @@
+import nao_nocv_2_1 as nao
+from naoqi import ALProxy
+import time
+
+robot_ip = "192.168.0.114"
+port = 9559  # Robot port number
+base_dir = "/home/nao/group_09/"
+
+class StateMachine():
+    def __init__(self, robot_ip, port, base_dir):
+        self.robot_ip = robot_ip
+        self.port = port
+        self.base_dir = base_dir
+        self.dialog_p = ALProxy('ALDialog', self.robot_ip, self.port)
+        self.memory_p = ALProxy('ALMemory', self.robot_ip, self.port)
+        self.dialog_p.setLanguage("English")
+
+    # Function definitions
+    def ActivateTopic(self, topf_path, module_name='MyModule'):
+        # Load topic - absolute path is required
+        topf_path = topf_path.decode('utf-8')
+        topic = self.dialog_p.loadTopic(topf_path.encode('utf-8'))
+
+        # Start dialog
+        self.dialog_p.subscribe(module_name)
+
+        # Activate dialog
+        self.dialog_p.activateTopic(topic)
+
+        return topic
+
+    def DeactivateTopic(self, topic, module_name='MyModule'):
+        # Deactivate topic
+        self.dialog_p.deactivateTopic(topic)
+
+        # Unload topic
+        self.dialog_p.unloadTopic(topic)
+
+        # Stop dialog
+        self.dialog_p.unsubscribe(module_name)
+
+    def DoStateInit(self):
+        """Do initialization"""
+        mylib.InitRobot()
+        return "My state A"
+
+    def StateRoaming(self, mystate):
+        """Execute state A"""
+        if mystate == "My state A":
+            mylib.behaviourA()
+            pass
+        elif mystate == "My state B":
+            pass
+        return "My state B"
+
+    def StateDetectedVisitor(self, mystate):
+        topf_path = base_dir + "greeting_enu.top"
+        topic = self.ActivateTopic(topf_path)
+
+        #Start timer
+        start_time = time.time()
+
+        while time.time() - start_time < 10:
+            # Check for speech detection
+            speech_detected = self.memory_p.getData("ALSpeechRecognition/Status")
+            if speech_detected:
+                start_time = time.time()
+                break
+            time.sleep(0.5)  # Check every 500 ms
+
+        # If no more interaction, move to other state of roaming around again
+        self.DeactivateTopic(topic)
+        return "Roaming"
+ 
+
+    def StateInteracting(self, mystate):
+        topf_path = base_dir + "greeting_enu.top"
+        topic = self.ActivateTopic(topf_path)
+
+        return None
+
+    def StateMovingVisitor(self, mystate):
+        """Execute state A"""
+        if mystate == "My state A":
+            mylib.behaviourA()
+            pass
+        elif mystate == "My state B":
+            pass
+        return "My state B"
+
+    # Main
+    def main(self):
+        # Initial state
+        state = "Roaming"
+
+        while state != "Done":
+            if state == "Roaming":
+                state = self.StateRoaming(state)
+            elif state == "Detected visitor":
+                state = self.StateDetectedVisitor(state)
+            elif state == "Interacting":
+                state = self.StateInteracting(state)
+            elif state == "Moving with visitor":
+                state = self.StateMovingVisitor(state)
+
+if __name__ == "__main__":
+    sm = StateMachine(robot_ip, port, base_dir)
+    sm.main()
