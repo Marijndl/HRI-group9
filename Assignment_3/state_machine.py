8a461a0e6d04583d927df07b2fb2cec4f03c1c56
Marijn <51094560+Marijndl@users.noreply.github.com>
2024-05-31 17:04:00 +0200

    Update state_machine.py

--- Assignment_3/state_machine.py
+++ Assignment_3/state_machine.py
@@ -2,7 +2,7 @@ import nao_nocv_2_1 as nao
 from naoqi import ALProxy
 import time
 
-robot_ip = "192.168.0.114"
+robot_ip = "192.168.0.116"
 port = 9559  # Robot port number
 base_dir = "/home/nao/group_09/"
 
@@ -14,6 +14,7 @@ class StateMachine():
         self.dialog_p = ALProxy('ALDialog', self.robot_ip, self.port)
         self.memory_p = ALProxy('ALMemory', self.robot_ip, self.port)
         self.dialog_p.setLanguage("English")
+        print("Setup the robot")
 
     # Function definitions
     def ActivateTopic(self, topf_path, module_name='MyModule'):
@@ -53,7 +54,7 @@ class StateMachine():
             pass
         elif mystate == "My state B":
             pass
-        return "My state B"
+        return "done"
 
     def StateDetectedVisitor(self, mystate):
         print("State: Detected visitor")
@@ -80,23 +81,31 @@ class StateMachine():
     def StateInteracting(self, mystate):
         print("State: Interacting with visitor")
 
-        topf_path = base_dir + "greeting_enu.top"
+        topf_path = base_dir + "PracticalQuestion_enu.top"
         topic = self.ActivateTopic(topf_path)
 
         #Start timer
-        start_time = time.time()
+        # start_time = time.time()
 
-        while time.time() - start_time < 10:
-            # Check for speech detection
-            speech_detected = self.memory_p.getData("ALSpeechRecognition/Status")
-            if speech_detected:
-                start_time = time.time()
+        # while abs(time.time() - start_time) < 100:
+            # Check if the variable is triggered]
+        moving_trigerred = self.memory_p.getData("Move")
+
+        while moving_trigerred != 1:
+            moving_trigerred = self.memory_p.getData("Move")
+
+            if moving_trigerred:
                 break
-            time.sleep(0.5)  # Check every 500 ms
 
+            time.sleep(0.5)  # Check every 500 ms
+        
+        
         # If no more interaction, move to other state of roaming around again
         self.DeactivateTopic(topic)
-        return "Roaming"
+        if moving_trigerred:
+            return "Moving with visitor"
+        else:
+            return "Roaming"
 
     def StateMovingVisitor(self, mystate):
         print("State: Moving with visitor")
@@ -106,9 +115,12 @@ class StateMachine():
     # Main
     def main(self):
         # Initial state
-        state = "Roaming"
+        state = "Interacting"
 
         while state != "Done":
+            
+            # last_command = self.memory_p.getData("Dialog/LatestCommand")
+
             if state == "Roaming":
                 state = self.StateRoaming(state)
             elif state == "Detected visitor":
@@ -117,7 +129,11 @@ class StateMachine():
                 state = self.StateInteracting(state)
             elif state == "Moving with visitor":
                 state = self.StateMovingVisitor(state)
+            # elif last_command == "quit":
+            #     break
+            
 
 if __name__ == "__main__":
     sm = StateMachine(robot_ip, port, base_dir)
     sm.main()
+    print("Quit the program")
