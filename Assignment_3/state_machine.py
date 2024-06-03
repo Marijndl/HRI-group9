b89cffbbefe98a2ff07de9b9cdef3dc830078110
StanleyGreen <s.j.j.groenen@student.tue.nl>
2024-06-03 23:15:14 +0200

    Update State_machine
    Everything between ###### is new and can be deleted if wanted.
    The function states_veerman is the general function that activates different states and communicates between them. Some function are addded as placeholders and still need its logic

--- Assignment_3/state_machine.py
+++ Assignment_3/state_machine.py
@@ -18,6 +18,101 @@ class StateMachine():
         self.topics = []
         print("Setup the robot")
 
+#####################################################################################################
+        self.current_state_active = None # 1 Roaminig, 2 Detected Visitor, 3 Interacting with user...
+        self.state_dict = {1: "Roaming State", 2: "Detected Visitor State", 3: "Interacting with User State", 4: "Moving with Visitor", 5: "Ending Interaction"}
+
+    def get_key_from_value(self, target_value):
+        for key, value in self.current_state_active.items():
+            if value == target_value:
+                return key
+        return None
+
+    def states_veerman(self):    # Veerman is a PSV player who dictates the plays
+        while True:
+            if self.current_state_active == 1:
+                next_move, info_for_next_move = self.State_Roaming(info_for_next_move)
+            
+            if self.current_state_active == 2:
+                next_move, info_for_next_move = self.State_Detected_Visitor(info_for_next_move)
+
+            if self.current_state_active == 3:
+                next_move, info_for_next_move = self.State_Interacting_User(info_for_next_move)
+            self.current_state_active = self.get_key_from_value(next_move)
+
+
+    def State_Roaming(self, info_for_next_move):
+        # Logic that makes the Robot does brr
+        face_detected, timestamp, location, face_info = nao.DetectFace() #for nao_nocv_2_1
+        if face_detected:
+            return "Detected Visitor State" , "Eye contact"
+        question_asked, question= self.question_detected()
+        if question_asked:
+            return "Interacting with User State", question
+    
+
+    def State_Detected_Visitor(self):
+        face_detected, timestamp, location, face_info = nao.DetectFace() #for nao_nocv_2_1
+        if face_detected:
+            start_time = time.time()
+        else:
+            return "Roaming State"
+        waiting = True
+        face_detected = False
+        bool_question_detected = False
+        while waiting:
+            if time.time() - start_time < 5:
+                waiting = False
+            elif time.time() - start_time < 2:
+                self.tablet_show_image("Smiley")
+            question_detected, question = self.question_detected()
+            if question_detected:
+                waiting = False
+            
+
+        face_detected, timestamp, location, face_info = nao.DetectFace()
+        if question_detected:
+            # A specific question is asked
+            return "Interacting with User State", question
+        elif face_detected:
+            # Turn to question
+            # "Hello my name is Eve! Welcome to our museum! You can ask me questions about museum pieces and museum utilities (?)"
+            return "Interacting with User State", "Introduce"        
+        else:
+            return "Roaming State", "Nothing" # No visitor and no question
+            
+        
+    def question_detected(self):
+        # Logic question detected
+        # if question_detected: THIS ONLY WHEN THERES A DIRECT QUESTION ABOUT THE MUSEUM
+        #     return True
+        # else:
+        #     return False
+
+    def tablet_show_image(self, image_path):
+        # Logic to show image on the tablet
+        # tabletService = ALProxy("ALTabletService", self.robot_ip, self.port)
+        # tabletService.showImageNoCache(image_path)
+
+
+    def State_Interacting_User(self, prev_move_info):
+        # if prev_move_info == "Introduce":
+        #     # Turn to question
+        #     # "Hello my name is Eve! Welcome to our museum! You can ask me questions about museum pieces and museum utilities (?)"
+        
+        # else:
+        #     prev_move_info == The question from prev
+    
+            
+
+
+
+
+    
+###############################################################################################        
+
+
+
     def ActivateTopic(self, topf_path, module_name='MyModule'):
         # Load topic - absolute path is required
         topf_path = topf_path.decode('utf-8')
@@ -110,6 +205,7 @@ class StateMachine():
                     self.DeactivateTopic(topic)
                 break
             
+
             # while state != "Done":
                 
             #     # last_command = self.memory_p.getData("Dialog/LatestCommand")
